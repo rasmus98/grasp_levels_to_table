ec1acd0fcbe3d0b0be7fdb1ab57c52c82e33e4e6
Leo Mulholland <leomulholland@leos-mbp.mp.qub.ac.uk>
2024-06-04 16:37:34 +0100

    avalue working again

--- avalue_table.py
+++ avalue_table.py
@@ -17,7 +17,7 @@ if args.file:
     csf_array,orb_labels,num_nrcsf,num_orbs,mode = create_csf_array_from_output(graspout)
     sorted_orbital_array,csf_sorted_by_orbital = sort_orbitals(orb_labels,orbitals_order,csf_array)
     csf_strings_prepared = make_csf_strings(2,csf_sorted_by_orbital,sorted_orbital_array,num_nrcsf)
-    map = find_relativistic_csfs(graspout,num_nrcsf)
+    map = find_relativistic_csfs(graspout,num_nrcsf)[0]
     states = find_levels(graspout,False,csf_strings_prepared,map)
 
 
